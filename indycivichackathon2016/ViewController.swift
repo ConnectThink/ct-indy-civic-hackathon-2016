2694bf59244f4d0d041acad63c735ad74a1fac9f
Matt Lathrop <mslathrop@gmail.com>
2016-06-04 11:36:05 -0400

    File new on iphone project

---
+++ indycivichackathon2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  indycivichackathon2016
+//
+//  Created by Matthew Lathrop on 6/4/16.
+//  Copyright © 2016 Connect Think LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
