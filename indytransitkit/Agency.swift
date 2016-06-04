efd84c435f4f69096bb17a3d498f2b1ce17b7cc4
Matt Lathrop <mslathrop@gmail.com>
2016-06-04 12:10:04 -0400

    Add model object for indy go

---
+++ indytransitkit/Agency.swift
@@ -0,0 +1,16 @@
+//
+//  Agency.swift
+//  indycivichackathon2016
+//
+//  Created by Matthew Lathrop on 6/4/16.
+//  Copyright © 2016 Connect Think LLC. All rights reserved.
+//
+
+import Foundation
+
+public struct Agency {
+    public var urlString = "ERROR"
+    public var name = "ERROR"
+    public var timezone = "ERROR"
+    public var id = "ERROR"
+}
---
+++ indytransitkit/StopTime.swift
@@ -0,0 +1,9 @@
+//
+//  StopTime.swift
+//  indycivichackathon2016
+//
+//  Created by Matthew Lathrop on 6/4/16.
+//  Copyright © 2016 Connect Think LLC. All rights reserved.
+//
+
+import Foundation
